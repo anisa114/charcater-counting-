38093c8755be2affef5c613f8766d1fb96dfdf15
Anisa Tahlil <anisa.tahlil22@gmaill.com>
2019-01-09 16:44:13 +0000

    nospaces string'

---
+++ count.js
@@ -0,0 +1,15 @@
+function countLetters(data){
+var object = {};
+var string = data.split(" ").join('').split('');
+console.log(string[0]);
+
+
+
+
+
+
+}
+
+//Test Case
+
+console.log(countLetters("lighthouse in the house"));
