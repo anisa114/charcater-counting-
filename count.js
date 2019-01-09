4032d0b748550efe7c53bf97c443080a5aae7494
Anisa Tahlil <anisa.tahlil22@gmaill.com>
2019-01-09 17:01:03 +0000

    final edit

--- count.js
+++ count.js
@@ -1,15 +1,17 @@
 function countLetters(data){
-var object = {};
-var string = data.split(" ").join('').split('');
-console.log(string[0]);
-
-
-
-
-
-
+  var object = {};
+  var string = data.split(" ").join('').split('');
+  console.log(string);
+  for(var i = 0; i < string.length; i++){
+    if(object[string[i]]=== undefined){
+      object[string[i]]=1;
+    }
+    else{
+      object[string[i]]++;
+    }
+  }
+  return object;
 }
 
 //Test Case
-
 console.log(countLetters("lighthouse in the house"));
